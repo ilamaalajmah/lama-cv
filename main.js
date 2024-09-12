a7f4a2b296c99b250137bc8365a73a3ade9794dd
Lama AlAjmah <ilamaabdullah77@gmail.com>
2024-09-12 04:50:11 +0300

    lama's cv

---
+++ main.js
@@ -0,0 +1,287 @@
+document.getElementById("number").onmouseover = function() {
+    this.textContent = "+966-50-821-6998"; 
+};
+document.getElementById("number").onmouseout = function() {
+    this.textContent = "+123-456-7890"; 
+};
+
+document.getElementById("email").onmouseover = function() {
+    this.textContent = "ilamaabdullah77@gmail.com"; 
+};
+document.getElementById("email").onmouseout = function() {
+    this.textContent = "hello@reallygreatsite.com"; 
+};
+
+document.getElementById("address").onmouseover = function() {
+    this.textContent = "Riyadh AlNarjs"; 
+};
+document.getElementById("address").onmouseout = function() {
+    this.textContent = "123 Anywhere St.,Any City"; 
+};
+
+document.getElementById("website").onmouseover = function() {
+    this.textContent = "www.lama.com"; 
+};
+document.getElementById("website").onmouseout = function() {
+    this.textContent = "www.reallygreatsite.com"; 
+};
+
+document.getElementById("year1").onmouseover = function() {
+    this.textContent = "2020-2023"; 
+};
+document.getElementById("year1").onmouseout = function() {
+    this.textContent = "2029-2030"; 
+};
+
+document.getElementById("uni-name").onmouseover = function() {
+    this.textContent = "AlBaha University"; 
+};
+document.getElementById("uni-name").onmouseout = function() {
+    this.textContent = "WARDIERE UNIVERSITY"; 
+};
+
+document.getElementById("degree1").onmouseover = function() {
+    this.textContent = "Bachelor of Computer Science"; 
+};
+document.getElementById("degree1").onmouseout = function() {
+    this.textContent = "Master of Business Management"; 
+};
+
+document.getElementById("year2").onmouseover = function() {
+    this.textContent = "2020-2023"; 
+};
+document.getElementById("year2").onmouseout = function() {
+    this.textContent = "2025-2029"; 
+};
+
+document.getElementById("uni").onmouseover = function() {
+    this.textContent = "University Of AlBaha"; 
+};
+document.getElementById("uni").onmouseout = function() {
+    this.textContent = "WARDIERE UNIVERSITY"; 
+};
+
+document.getElementById("degree2").onmouseover = function() {
+    this.textContent = "Bachelor of Computer Science"; 
+};
+document.getElementById("degree2").onmouseout = function() {
+    this.textContent = "Bachelor of Business"; 
+};
+
+document.getElementById("gpa").onmouseover = function() {
+    this.textContent = "GPA: 3.94 / 4.0"; 
+};
+document.getElementById("gpa").onmouseout = function() {
+    this.textContent = "GPA: 3.8 / 4.0"; 
+};
+
+document.getElementById("s1").onmouseover = function() {
+    this.textContent = "Problem Solving"; 
+};
+document.getElementById("s1").onmouseout = function() {
+    this.textContent = "Project Management"; 
+};
+
+document.getElementById("s2").onmouseover = function() {
+    this.textContent = "Leadership"; 
+};
+document.getElementById("s2").onmouseout = function() {
+    this.textContent = "Public Relations"; 
+};
+
+document.getElementById("s3").onmouseover = function() {
+    this.textContent = "Flexibility"; 
+};
+document.getElementById("s3").onmouseout = function() {
+    this.textContent = "Teamwork"; 
+};
+
+document.getElementById("s4").onmouseover = function() {
+    this.textContent = "Communication Skills"; 
+};
+document.getElementById("s4").onmouseout = function() {
+    this.textContent = "Time Management"; 
+};
+
+document.getElementById("s5").onmouseover = function() {
+    this.textContent = "Attention to Detail"; 
+};
+document.getElementById("s5").onmouseout = function() {
+    this.textContent = "Leadership"; 
+};
+
+document.getElementById("s6").onmouseover = function() {
+    this.textContent = "Time Management"; 
+};
+document.getElementById("s6").onmouseout = function() {
+    this.textContent = "Effective Communication"; 
+};
+
+document.getElementById("s7").onmouseover = function() {
+    this.textContent = "Teamwork"; 
+};
+document.getElementById("s7").onmouseout = function() {
+    this.textContent = "Critical Thinking"; 
+};
+
+document.getElementById("lang1").onmouseover = function() {
+    this.textContent = "Italian (Fluent)"; 
+};
+document.getElementById("lang1").onmouseout = function() {
+    this.textContent = "English (Fluent)"; 
+};
+
+document.getElementById("lang2").onmouseover = function() {
+    this.textContent = "Spanish (Intermediate)"; 
+};
+document.getElementById("lang2").onmouseout = function() {
+    this.textContent = "French (Fluent)"; 
+};
+
+document.getElementById("lang3").onmouseover = function() {
+    this.textContent = "Chinese (Basics)"; 
+};
+document.getElementById("lang3").onmouseout = function() {
+    this.textContent = "German (Basics)"; 
+};
+
+document.getElementById("lang4").onmouseover = function() {
+    this.textContent = "Arabic (Intermediate)"; 
+};
+document.getElementById("lang4").onmouseout = function() {
+    this.textContent = "Spanish (Intermediate)"; 
+};
+document.getElementById("profile").onmouseover = function() {
+    this.textContent = "As a Front-end Web Developer with a Bachelor's degree in Computer Science, I bring a solid foundation in both theoretical knowledge and practical skills. Throughout my academic and professional journey, I've undertaken various projects showcasing my skills and creativity."; 
+};
+document.getElementById("profile").onmouseout = function() {
+    this.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur dolor voluptatibus hic maiores, laboriosam quibusdam eos labore cum, aliquid similique harum vitae eius vel aut! Deserunt tempora explicabo voluptas repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque doloremque odio consequuntur laudantium sit beatae ducimus veniam ratione reprehenderit. Architecto tempora, ullam voluptatem quidem repellendus molestiae fugiat! Dolorum, nemo ratione?"; 
+};
+
+document.getElementById("range1").onmouseover = function() {
+    this.textContent = "Chief Executive Officer (2023–2023)"; 
+};
+document.getElementById("range1").onmouseout = function() {
+    this.textContent = "Borcelto Studio (2030 - Present)"; 
+};
+
+document.getElementById("depar1").onmouseover = function() {
+    this.textContent = "Mofradah Company"; 
+};
+document.getElementById("depar1").onmouseout = function() {
+    this.textContent = "Marketing Manager & Specialist"; 
+};
+
+document.getElementById("range2").onmouseover = function() {
+    this.textContent = "Google Developer Student Club 2022-2023"; 
+};
+document.getElementById("range2").onmouseout = function() {
+    this.textContent = "Fauget Studio 2025-2029"; 
+};
+
+document.getElementById("depar2").onmouseover = function() {
+    this.textContent = "Quality Specialist"; 
+};
+document.getElementById("depar2").onmouseout = function() {
+    this.textContent = "Marketing Manager & Specialist"; 
+};
+
+document.getElementById("range3").onmouseover = function() {
+    this.textContent = "AlBaha University 2020-2023"; 
+};
+document.getElementById("range3").onmouseout = function() {
+    this.textContent = "Studio Shodwe 2024-2025"; 
+};
+
+document.getElementById("depar3").onmouseover = function() {
+    this.textContent = "Fresh Graduate"; 
+};
+document.getElementById("depar3").onmouseout = function() {
+    this.textContent = "Marketing Manager & Specialist"; 
+};
+
+
+document.getElementById("tasks1").onmouseover = function() {
+    this.innerHTML = `
+        <li>Provide leadership, mentorship, and guidance to ensure team cohesion and productivity.</li>
+        <li>Participation in Children's and Young Adult Literature Accelerator.</li>
+        <li>Act as a liaison between the technical team and customer support representatives.</li>
+    `;
+};
+document.getElementById("tasks1").onmouseout = function() {
+    this.innerHTML = `
+        <li>Develop and execute comprehensive marketing strategies and campaigns that align with the company's goals and objectives.</li>
+        <li>Lead, mentor, and manage a high-performing marketing team, fostering a collaborative and results-driven work environment.</li>
+        <li>Monitor brand consistency across marketing channels and materials.</li>
+    `;
+};
+
+document.getElementById("tasks2").onmouseover = function() {
+    this.innerHTML = `
+        <li>Following up on the students’ evaluation of the courses offered by the club.</li>
+        <li>Analyze and report on the performance of marketing campaigns, adjusting strategies as needed to optimize results.</li>
+        <li>Develop and maintain strong relationships with key stakeholders, including clients, partners, and media contacts.</li>
+    `;
+};
+document.getElementById("tasks2").onmouseout = function() {
+    this.innerHTML = `
+        <li>Manage client relationships and ensure high levels of customer satisfaction.</li>
+        <li>Documenting the number of volunteer work hours and recording them for each event.</li>
+        <li>Expressing opinions and suggestions for developing the club’s programs.</li>
+    `;
+};
+
+document.getElementById("tasks3").onmouseover = function() {
+    this.innerHTML = `
+        <li>Graduated with First Class Honours in Bachelor's degree</li>
+        <li>working and professional development activities</li>
+        
+    `;
+};
+document.getElementById("tasks3").onmouseout = function() {
+    this.innerHTML = `
+        <li>Develop and execute marketing campaigns and initiatives that support company goals and drive revenue growth.</li>
+        <li>Lead the development of marketing materials, including website content, brochures, and promotional materials.</li>
+        <li>Manage and oversee external agencies and vendors to ensure successful execution of marketing projects.</li>
+    `;
+};
+
+document.getElementById("referenceinfo1").onmouseover = function() {
+    this.innerHTML = `
+        <li><h4>Lama Alajmah</h4></li>
+        <li>Stc inc. / CTO</li>
+        <li>phone: +966-50-821-6998</li>
+        <li>Email: lama@reallygreatsite.co</li>
+    `;
+};
+document.getElementById("referenceinfo1").onmouseout = function() {
+    this.innerHTML = `
+        <li><h4>Estelle Darcy</h4></li>
+        <li>Wardiere inc. / CTO</li>
+        <li>phone: 123-456-7890</li>
+        <li>Email: hello@reallygreatsite.com</li>
+    `;
+};
+
+document.getElementById("referenceinfo2").onmouseover = function() {
+    this.innerHTML = `
+        <li><h4>Intern</h4></li>
+        <li>tuwaiq inc. / CTO</li>
+        <li>phone: +966-50-821-6998</li>
+        <li>Email: lhello@reallygreatsite.com</li>
+    `;
+};
+document.getElementById("referenceinfo2").onmouseout = function() {
+    this.innerHTML = `
+        <li><h4>Harper Richard</h4></li>
+        <li>Wardiere inc. / CTO</li>
+        <li>phone: 123-456-7890</li>
+        <li>Email: hello@reallygreatsite.com</li>
+    `;
+};
+document.getElementById("name").onmouseover = function() {
+    this.innerHTML = "Lama <span>Aljmah</span>"; 
+};
+document.getElementById("name").onmouseout = function() {
+    this.innerHTML = "RICHARD <span>SANCHEZ</span>"; 
+};
